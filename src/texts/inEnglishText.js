c856a7d9440b2b4ca4da5d3cb2ee9e09436aced0
Sofia <sofia.montgomery@gmail.com>
2025-01-23 01:13:59 +0100

    edit text-files

--- src/texts/inEnglishText.js
+++ src/texts/inEnglishText.js
@@ -1,5 +1,5 @@
 export const inEnglishText = {
   heading: "Hi!",
   content:
-    "My name is Sofia Montgomery and I live in Sweden! This is my portfolio!",
+    "Welcome to my portfolio! My name is Sofia Montgomery and I live in Sweden. I'm a educated fullstack developer and an aspiring web designer. Check out my Projekt page, there you can see some of my projects in web development and design! Want an explanation? Please contact me by using the contact form, email, or go to LinkedIn.",
 };
