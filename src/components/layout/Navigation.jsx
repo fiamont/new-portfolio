91006691fda7105c64aa6e85f873c64d77e8af63
Sofia <sofia.montgomery@gmail.com>
2025-01-24 22:29:21 +0100

    finetuning navigation

--- src/components/layout/Navigation.jsx
+++ src/components/layout/Navigation.jsx
@@ -3,11 +3,28 @@ import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
+import { useState } from "react";
 
 const StyledNavbar = styled(Navbar)`
   background: none;
+
   .navbar-toggler {
     border: none;
+    &:focus {
+      outline: none;
+      box-shadow: none;
+    }
+  }
+
+  .navbar-collapse {
+    text-align: center;
+  }
+
+  .nav-link {
+    color: var(--text-color);
+    &:hover {
+      color: var(--red-color);
+    }
   }
 `;
 
@@ -16,29 +33,48 @@ const StyledContainer = styled(Container)`
 `;
 
 function Navigation() {
+  const [expanded, setExpanded] = useState(false);
+
+  const handleToggle = () => {
+    const newExpanded = !expanded;
+    console.log("Hamburgermenyns tillstånd:", newExpanded);
+    setExpanded(newExpanded);
+  };
+
+  const handleClose = () => {
+    console.log("Hamburgermenyn stängs!");
+    setExpanded(false);
+  };
+
+  console.log("Hamburgermenyn är öppen:", expanded);
+
   return (
     <StyledNavbar
       collapseOnSelect
       expand="lg"
       className="p-2 p-sm-3 p-md-4 p-lg-5"
+      expanded={expanded}
     >
       <StyledContainer>
         <Navbar.Brand as={Link} to={"/"}>
           Hem
         </Navbar.Brand>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Toggle
+          aria-controls="responsive-navbar-nav"
+          onClick={handleToggle}
+        />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="ms-auto">
-            <Nav.Link as={Link} to={"/about"}>
+            <Nav.Link as={Link} to={"/about"} onClick={handleClose}>
               Om mig
             </Nav.Link>
-            <Nav.Link as={Link} to={"/projects"}>
+            <Nav.Link as={Link} to={"/projects"} onClick={handleClose}>
               Projekt
             </Nav.Link>
-            <Nav.Link as={Link} to={"/inEnglish"}>
+            <Nav.Link as={Link} to={"/inEnglish"} onClick={handleClose}>
               In English
             </Nav.Link>
-            <Nav.Link as={Link} to={"/contact"}>
+            <Nav.Link as={Link} to={"/contact"} onClick={handleClose}>
               Kontakt
             </Nav.Link>
           </Nav>
