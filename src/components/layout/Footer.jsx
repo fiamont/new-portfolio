380342782c34d8a4bbe5bca1648fc4a130dbc467
Sofia <sofia.montgomery@gmail.com>
2025-01-25 00:26:19 +0100

    edit portfolio-text and icon-linking

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,4 +1,3 @@
-import styled from "styled-components";
 import { FaLinkedin, FaGithub } from "react-icons/fa";
 
 function Footer() {
@@ -6,8 +5,22 @@ function Footer() {
     <div>
       <footer className="container-fluid text-center p-2 p-sm-3 p-md-4 p-lg-5">
         <div>
-          <FaLinkedin className="mx-2 fs-1 fs-lg-2" />
-          <FaGithub className="mx-2 fs-1 fs-lg-2" />
+          <a
+            href="https://github.com/fiamont"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="mx-2 fs-1 fs-lg-2"
+          >
+            <FaGithub />
+          </a>
+          <a
+            href="https://www.linkedin.com/in/sofia-montgomery-3175a821a"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="mx-2 fs-1 fs-lg-2"
+          >
+            <FaLinkedin />
+          </a>
         </div>
         <p className="pt-2">
           &copy; {new Date().getFullYear()} Sofia Montgomery portfolio
