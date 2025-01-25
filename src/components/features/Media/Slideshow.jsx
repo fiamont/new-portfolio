8420beb1fd63dc7398b1bc39c30ca3c2cf18140d
Sofia <sofia.montgomery@gmail.com>
2025-01-25 05:54:35 +0100

    added working carousel for slideshow

--- src/components/features/Media/Slideshow.jsx
+++ src/components/features/Media/Slideshow.jsx
@@ -1,17 +1,56 @@
-import bollarSquare from "./../../../assets/examensprojektet.png";
 import styled from "styled-components";
+import { useState } from "react";
+import Carousel from "react-bootstrap/Carousel";
+import vaxtverketDesign from "./../../../assets/vaxtverketDesign.png";
+import examensprojektet from "./../../../assets/examensprojektet.png";
+import awesomeMemory from "./../../../assets/awesomeMemory.png";
+import { Row, Col } from "react-bootstrap";
 
 const StyledDiv = styled.div`
-  background-color: var(--yellow-color);
+  background-color: rgb(242, 159, 5, 0.7);
   border-radius: 10px;
 `;
 
+const StyledCarousel = styled(Carousel)`
+  .carousel-control-next-icon,
+  .carousel-control-prev-icon {
+    border-radius: 50%;
+  }
+
+  .carousel-indicators,
+  .carousel-caption {
+    background: none !important;
+  }
+`;
+
 function Slideshow() {
+  const [index, setIndex] = useState(0);
+
+  const handleSelect = (selectedIndex) => {
+    setIndex(selectedIndex);
+  };
+
   return (
-    <StyledDiv className="d-flex flex-column text-center p-3">
-      <h2 className="pb-2">(Slideshow)</h2>
-      <img src={bollarSquare} alt="balls" />
-    </StyledDiv>
+    <Row className="justify-content-center ">
+      <Col xs={11} sm={9} md={6}>
+        <StyledDiv className="d-flex flex-column text-center p-2 p-md-8">
+          <StyledCarousel fade activeIndex={index} onSelect={handleSelect}>
+            <Carousel.Item>
+              <img src={examensprojektet} text="examensprojektet" />
+              <Carousel.Caption></Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+              <img src={awesomeMemory} text="awesome memory" />
+              <Carousel.Caption></Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+              <img src={vaxtverketDesign} text="växtverket" />
+              <Carousel.Caption></Carousel.Caption>
+            </Carousel.Item>
+          </StyledCarousel>
+        </StyledDiv>
+      </Col>
+    </Row>
   );
 }
 
