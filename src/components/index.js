1baf21dee8b9210c24e4df55b861810d77006713
Sofia <sofia.montgomery@gmail.com>
2025-01-28 16:36:17 +0100

    removed unnecessary components and folders

--- src/components/index.js
+++ src/components/index.js
@@ -2,15 +2,11 @@
 export { default as ContactForm } from "./features/Contact/ContactForm.jsx";
 export { default as Slideshow } from "./features/Media/Slideshow.jsx";
 export { default as VideoPlayer } from "./features/Media/VideoPlayer.jsx";
-export { default as ProgrammingPart } from "./features/Project/ProgrammingPart.jsx";
-export { default as WebDesignPart } from "./features/Project/WebDesignPart.jsx";
 
 //layout
 export { default as Navigation } from "./layout/Navigation.jsx";
 export { default as Footer } from "./layout/Footer.jsx";
-export { default as FullScreenLine } from "./layout/FullScreenLine.jsx";
 export { default as Layout } from "./layout/Layout.jsx";
-export { default as VerticalLine } from "./layout/VerticalLine.jsx";
 
 // ui
 export { default as TextBox } from "./ui/TextBox.jsx";
